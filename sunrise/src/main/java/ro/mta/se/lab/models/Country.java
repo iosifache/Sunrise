ea7cb2eba8b2bec1884d8dc0c260ed706843f080
George-Andrei Iosif <georgeiosif@live.com>
2021-01-06 11:38:12 +0200

    Implements location binding in the user interface
    Implements the binding of the locations (countries and corresponding
    cities), that are saved into a JSON file, to the list presented
    in the user interface. The implementation is realized regarding to
    MVC pattern.
    
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

---
+++ sunrise/src/main/java/ro/mta/se/lab/models/Country.java
@@ -0,0 +1,33 @@
+package ro.mta.se.lab.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Country implements ILocation {
+
+  String name;
+  List<City> cityList = new ArrayList<City>();
+
+  public Country(String name, List<City> cityList){
+    this.name = name;
+    this.cityList = cityList;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public List<City> getCityList() {
+    return cityList;
+  }
+
+  @Override
+  public String toString() {
+    return this.getName();
+  }
+
+  public Long getCityId(){
+    return (long)-1;
+  }
+
+}
---
+++ sunrise/src/main/java/ro/mta/se/lab/models/ILocation.java
@@ -0,0 +1,7 @@
+package ro.mta.se.lab.models;
+
+public interface ILocation {
+  String getName();
+  String toString();
+  Long getCityId();
+}
