8a685d3814cf0e869d63a2fc88841d2220340b32
George-Andrei Iosif <georgeiosif@live.com>
2021-01-06 09:16:30 +0200

    Creates user interface and window controller
    Creates a minimal user interface, containing all required elements,
    and a controller for actions such as window moving, closing,
    minimizing and location binding.
    
    Signed-off-by: George-Andrei Iosif <georgeiosif@live.com>

---
+++ sunrise/src/main/java/ro/mta/se/lab/Main.java
@@ -0,0 +1,30 @@
+package ro.mta.se.lab;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  public void start(Stage primaryStage) {
+    FXMLLoader loader = new FXMLLoader();
+    try {
+      loader.setLocation(this.getClass().getResource("/view/MainView.fxml"));
+      primaryStage.setScene(new Scene(loader.load()));
+      primaryStage.initStyle(StageStyle.TRANSPARENT);
+      primaryStage.setResizable(false);
+      primaryStage.show();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+
+}
